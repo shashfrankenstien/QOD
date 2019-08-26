9fdf3cc2c91985edad41e44730a4ee8fb41b9e40
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2019-08-26 01:53:36 -0400

    Fetch 60

--- qod/quotes.py
+++ qod/quotes.py
@@ -10,7 +10,7 @@ headers = {
 	'Referer': 'https://www.quotery.com/quotes',
 	'Origin': 'https://www.quotery.com',
 }
-url = 'https://api.quotery.com/wp-json/quotery/v1/quotes?orderby={sort}&page=1&per_page=20'
+url = 'https://api.quotery.com/wp-json/quotery/v1/quotes?orderby={sort}&page=1&per_page=60'
 
 sorts = dict(
 	random = 'random',
